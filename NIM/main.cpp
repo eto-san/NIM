f2b17e18ade4f2a1b7a4b735219abebd890eebe1
EshaO <eTo@eshaos-mbp.lan>
2013-11-28 11:51:30 -0800

    Initial Commit

---
+++ NIM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NIM
+//
+//  Created by EshaO on 11/28/13.
+//  Copyright (c) 2013 EshaO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
